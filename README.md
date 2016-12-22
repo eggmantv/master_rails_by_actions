9b978aebc0d1eadcfdc5ecf770c9d8e0b9d8fc46
Lei Lee <mytake6@gmail.com>
2016-12-22 16:05:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# eggman.tv
+
+**[精通Rails实战开发](http://eggman.tv/c/s-master-rails-by-actions)课程源码**
+
+为了方便在每节课的代码间进行切换，该课程是通过git tag的方式来组织的，就是每节课对应一个git tag。
+
+首先clone该项目
+
+```shell
+$git clone git@github.com:eggmantv/master_rails_by_actions.git
+```
+
+切换tag
+```shell
+$cd master_rails_by_actions
+
+查看所有标签
+$git tag
+01
+02
+...
+
+切换到第一节课的源码
+$git checkout 01
+```
+
+©EGGMAN.TV 蛋人网
+
---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MasterRailsByActions
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |generator|
+      generator.assets false
+      generator.test_framework false
+    end
+    
+  end
+end
